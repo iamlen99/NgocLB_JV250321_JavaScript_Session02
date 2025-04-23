e98d75a16f6efb17e4d10f64d1a0deb0c315ed14
iamlen99 <allblue2903@gmail.com>
2025-04-23 21:41:41 +0700

    Upload bài tập session 02 - 1

---
+++ exercise 01/script.js
@@ -0,0 +1,6 @@
+let integerNumber = +prompt("Enter an integer:");
+if (integerNumber % 2 === 0) {
+  console.log("The number you entered is even!");
+} else {
+  console.log("The number you entered is odd!");
+}
